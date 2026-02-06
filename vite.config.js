4d2e663f067a2f3785f79532154110bda459d8a7
Sergiy Humenniy <gum0372@gmail.com>
2026-02-06 21:16:45 +0200

    upgrade vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import SortCss from 'postcss-sort-media-queries';
 
 export default defineConfig(({ command }) => {
   return {
+    base: '/modern-bakery-ua/',
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
